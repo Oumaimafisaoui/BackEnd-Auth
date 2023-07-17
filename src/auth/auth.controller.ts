591354e5b7417d0dc5876a5cb1808e6913ea037b
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-07-17 14:03:47 +0100

    jwt added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthDto } from 'src/dto';
 import { AuthService } from './auth.service';
-@Controller({})
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
